0480d6e68b4ee347227975e5f0484e68ad7714d3
Lev2307 <levchik23072007@gmail.com>
2022-04-15 18:29:40 +0300

    upd delete, update

--- kidkodNotesApp/notes/forms.py
+++ kidkodNotesApp/notes/forms.py
@@ -32,3 +32,30 @@ class CreateNoteModelForm(forms.ModelForm):
             'body': 'Описание',
             'status': 'Статус',
         }
+
+class EditNoteModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline'
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Field('header', css_class="form-control mb-2", PlaceHolder="Заголовок..."),
+            Field('body', css_class="form-control mb-2", PlaceHolder="Описание..."),
+            Field('status', css_class="form-check-input mb-2 text-info"),
+            ButtonHolder (
+                Submit('edit', 'Изменить', css_class='btn btn-warning mb-2 fw-normal')
+            )
+        )
+    class Meta:
+        model = NotesModel
+        fields = [
+            'header',
+            'body',
+            'status'
+        ]
+        labels = {
+            'header': 'Заголовок',
+            'body': 'Описание',
+            'status': 'Статус',
+        }
